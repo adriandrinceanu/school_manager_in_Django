80e48c582415e83ebd81737858b1574b66136f01
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-07 11:46:16 +0200

    models draft

--- school_manager/core/models.py
+++ school_manager/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Principal(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return f"Principal {self.name}"
+    
+class Student(models.Model):
+    pass
+    
+class Year(models.Model):
+    year = models.IntegerField(max_length=1)
+    students = models.ForeignKey(Student)
+    
+
+class Teacher(models.Model):
+    pass
+
+class Class(models.Model):
+    pass
+
+class Course(models.Model):
+    pass
+
+class Grade(models.Model):
+    pass
+
+class Parent(models.Model):
+    pass
+
+
