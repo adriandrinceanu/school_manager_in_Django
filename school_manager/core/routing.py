cf888f225508d8767b0e96aadb17fef97c2365b1
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-26 16:28:17 +0200

    secure websocket

--- school_manager/core/routing.py
+++ school_manager/core/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<username>\w+)_(?P<username2>\w+)_chatroom/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<username>\w+)_(?P<username2>\w+)_chatroom/$', consumers.ChatConsumer.as_asgi()),
 ]
