0a4508dd33f718c65a256b3614e3a1414622828a
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-26 16:08:08 +0200

    trying to fix postgres live on render

--- school_manager/docker_entrypoint.sh
+++ school_manager/docker_entrypoint.sh
@@ -5,4 +5,5 @@ python manage.py makemigrations
 python manage.py migrate 
 python manage.py createsuperuser --no-input
 python manage.py collectstatic --no-input
+python manage.py loaddata db.json
 gunicorn school_manager.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
